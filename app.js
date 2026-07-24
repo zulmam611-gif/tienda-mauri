6769ef7066d39280291025ddf81f74f26bb9eea6
Mauri <zulmam611@gmail.com>
2026-07-24 09:54:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -317,9 +317,6 @@ MOSTRAR PRODUCTOS EN LA TIENDA
 ==================================================
 */
 
-const listaProductos =
-  document.getElementById("listaProductos");
-
 function renderizarProductos() {
 
   let resultado = [...productos];
