7f53605aa698e095a208271a713a648513756376
Mauri <zulmam611@gmail.com>
2026-07-24 05:17:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -217,12 +217,6 @@ const iniciarSesionAdministrador =
 const mensajeLogin =
   document.getElementById("mensajeLogin");
 
-const abrirLoginAdmin = document.getElementById("abrirLoginAdmin");
-
-abrirLoginAdmin.addEventListener("click", () => {
-  modalLogin.style.display = "flex";
-});
-
 cerrarLogin.addEventListener("click", () => {
   modalLogin.style.display = "none";
 });
